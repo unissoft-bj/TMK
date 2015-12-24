32807f1ec58659d6a16f273282f749871a980881
michaelyin <gt8442b@yahoo.com>
2015-12-23 22:27:54 -0500

    jpa repository for data accessing.

---
+++ src/main/java/net/wyun/dianxiao/repository/primary/PrimaryRepository.java
@@ -0,0 +1,10 @@
+package net.wyun.dianxiao.repository.primary;
+
+import net.wyun.dianxiao.model.primary.PrimaryModel;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PrimaryRepository extends CrudRepository<PrimaryModel, Integer>{
+
+}
---
+++ src/main/java/net/wyun/dianxiao/repository/secondary/SecondaryRepository.java
@@ -0,0 +1,10 @@
+package net.wyun.dianxiao.repository.secondary;
+
+import net.wyun.dianxiao.model.secondary.SecondaryModel;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface SecondaryRepository extends CrudRepository<SecondaryModel, Integer> {
+
+}
