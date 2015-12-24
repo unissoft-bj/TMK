b7695a71397006ff9f215e330464cb644a7208c8
michaelyin <gt8442b@yahoo.com>
2015-12-23 22:27:17 -0500

    data models for the two datasources.

---
+++ src/main/java/net/wyun/dianxiao/model/primary/PrimaryModel.java
@@ -0,0 +1,37 @@
+package net.wyun.dianxiao.model.primary;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PRIMARY_MODEL")
+public class PrimaryModel {
+	
+	@Id
+	@Column(name = "ID", unique = true, nullable = false)
+	@SequenceGenerator(name = "PRIMARY_MODEL_ID", sequenceName = "PRIMARY_MODEL_SEQ_ID", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRIMARY_MODEL_ID")
+	private int id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
