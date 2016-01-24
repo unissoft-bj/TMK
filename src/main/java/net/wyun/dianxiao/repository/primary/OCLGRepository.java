805fa250a7481f7cd9b5877f842d0f6ac2f4cb81
michaelyin <gt8442b@yahoo.com>
2016-01-24 08:38:49 -0500

    add function to query max clgcode.

--- src/main/java/net/wyun/dianxiao/repository/primary/OCLGRepository.java
+++ src/main/java/net/wyun/dianxiao/repository/primary/OCLGRepository.java
@@ -3,9 +3,12 @@ package net.wyun.dianxiao.repository.primary;
 import net.wyun.dianxiao.model.primary.OCLG;
 import net.wyun.dianxiao.model.primary.PrimaryModel;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface OCLGRepository extends CrudRepository<OCLG, Integer>{
 
+	@Query("select coalesce(max(o.clgCode), '0') from OCLG o")
+	int findMaxClgCode();
 }
