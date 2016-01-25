d6859d02a9b55969fd71a1c8599dcc96db29b8b9
michaelyin <gt8442b@yahoo.com>
2016-01-24 22:54:49 -0500

    only handle CREATE event. delay 4 seconds before moving mp3 files.

--- src/main/java/net/wyun/dianxiao/watcher/SalesCallMonitor.java
+++ src/main/java/net/wyun/dianxiao/watcher/SalesCallMonitor.java
@@ -3,12 +3,22 @@
  */
 package net.wyun.dianxiao.watcher;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent.Kind;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
+import net.wyun.dianxiao.model.CallDirection;
+import net.wyun.dianxiao.service.secondary.SalesAgentLookUpService;
+
+import java.nio.file.Files;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,52 +31,107 @@ import com.sentaca.watcher.DirectoryEventListenerAdapter;
  */
 @Service
 public class SalesCallMonitor {
-	
-	@Value("${dianxiao.call.directory}")
+
+	@Value("${dianxiao.record.directory}")
 	private String directory;
-	
+
+	@Value("${dianxiao.target.directory}")
+	private String targetDir;
+
 	DirectoryWatchService directoryWatchService;
-	
-	public SalesCallMonitor(){
-		System.out.println("create SalesCallMonitor.");
-		
+	private static final Logger logger = LoggerFactory
+			.getLogger(SalesCallMonitor.class);
+
+	@Autowired
+	ActivityPersistor activityPersistor;
+
+	public SalesCallMonitor() {
+		logger.info("create SalesCallMonitor.");
+
 	}
-	
-	private void createDirWatcher(){
-		directoryWatchService = new DirectoryWatchService(new DirectoryEventListenerAdapter() {
 
-		      @Override
-		      public void onEvent(Kind<Path> kind, Path path) {
-		        System.out.println(kind + " " + path);
-		      }
+	private void createDirWatcher() {
+		directoryWatchService = new DirectoryWatchService(
+				new DirectoryEventListenerAdapter() {
 
-		      @Override
-		      public void onNewWatcher(String dir) {
-		        System.out.println("New watcher for " + dir);
-		      }
+					@Override
+					public void onEvent(Kind<Path> kind, Path path) {
+						// System.out.println(kind + " " + path);
+						logger.info(kind + ", " + path + ", is absolute path: "	+ path.isAbsolute());
+						if(!kind.toString().equals("ENTRY_CREATE")) return;
+						if (!FileProcessUtil.isFile(path)) 			return;
+						if (!path.toString().endsWith(".mp3"))		return;
 
-		      @Override
-		      public void onWatcherClosed(String dir) {
-		        System.out.println("Watcher removed: " + dir);
-		      }
-		    }, this.directory);
+						// we have a mp3 file
+						// 1. remove those internal calls
+						String fileName = FileProcessUtil.extractFileName(path
+								.toString());
+						List<String> list = FileProcessUtil.process(fileName);
+						CallDirection direction = activityPersistor.getCallDirection(list);
+						
+						if (direction == CallDirection.INTERNAL) {
+							// remove audio file
+							try {
+								Files.delete(path);
+							} catch (IOException e) {
+								logger.error(e.getLocalizedMessage());
+								e.printStackTrace();
+							}
+							return;
+
+						}
+
+						// either IN or OUT, an activity should be recorded
+						// move audio to target dir
+						String source = path.toString();
+						String target = source.replace(directory, targetDir);
+						
+						//first persist data to database
+						list.add(target);
+						logger.info("persist activity now ...");
+						activityPersistor.persist(direction, list);
+						logger.info("persist activity done.");
+						
+						try {
+							TimeUnit.SECONDS.sleep(4);
+						} catch (InterruptedException e1) {		}
+						
+						try {
+							FileProcessUtil.move(source, target);
+							logger.info("moving file done. file now at: " + target);
+						} catch (IOException e) {
+							logger.error(e.getLocalizedMessage());
+							e.printStackTrace();
+						}
+						
+
+					}
+
+					@Override
+					public void onNewWatcher(String dir) {
+						logger.info("New watcher for " + dir);
+					}
+
+					@Override
+					public void onWatcherClosed(String dir) {
+						logger.info("Watcher removed: " + dir);
+					}
+				}, this.directory);
 	}
-	
+
 	public void startWatching() throws Exception {
-		
+
 		directoryWatchService.watch();
 
-	    //Thread.sleep(10000000);
-	    System.out.println("waiting for user input to create event at /tmp/root!");
-	    //TimeUnit.SECONDS.sleep(60);
-	  }
-	
+		// Thread.sleep(10000000);
+		logger.info("monitoring directory: " + directory);
+		// TimeUnit.SECONDS.sleep(60);
+	}
+
 	@PostConstruct
-    public void init() throws Exception {
+	public void init() throws Exception {
 		this.createDirWatcher();
 		startWatching();
 	}
-	
-	
 
 }
