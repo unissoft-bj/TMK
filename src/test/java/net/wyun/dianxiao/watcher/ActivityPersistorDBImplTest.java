0b0f45c61da398f95f9124b3a5ce5500bd5e810c
michaelyin <gt8442b@yahoo.com>
2016-01-24 22:58:35 -0500

    unit test of ActivityPersistorDBImpl class.

---
+++ src/test/java/net/wyun/dianxiao/watcher/ActivityPersistorDBImplTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package net.wyun.dianxiao.watcher;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import net.wyun.dianxiao.BaseSpringTestRunner;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author michael
+ *
+ */
+public class ActivityPersistorDBImplTest extends BaseSpringTestRunner {
+	
+	@Autowired
+	ActivityPersistorDBImpl activityPersistor;
+
+	@Test
+	public void testGetEndTime() {
+		int endTime = activityPersistor.getEndTime(new Date());
+		System.out.println("current time (hour minute: " + endTime);
+	}
+
+}
