a8de9e7598fa65dd847a1192e75705e72306636c
michaelyin <gt8442b@yahoo.com>
2016-02-02 23:23:59 -0500

    move the main class one level lower.

--- src/main/java/net/wyun/dianxiao/DianxiaoApplication.java
+++ src/main/java/net/wyun/dianxiao/DianxiaoApplication.java
@@ -1,12 +1,17 @@
-package net.wyun;
+package net.wyun.dianxiao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DianxiaoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DianxiaoApplication.class);
     public static void main(String[] args) {
+    	logger.info("start Dianxiao Application now ...");
         SpringApplication.run(DianxiaoApplication.class, args);
+        logger.info("Dianxiao Application fully loaded now.");
     }
 }
