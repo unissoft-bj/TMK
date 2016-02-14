db54d27c795c3b3a5f43fa2cb1c39d9e3c324df0
michaelyin <gt8442b@yahoo.com>
2016-02-14 11:40:20 -0500

    script to create OATC table. script to update FAX for OUSR.

---
+++ src/main/resources/db/migration/primary/V1455453863__PRIMARY_MODEL_INSERT.sql
@@ -0,0 +1,3 @@
+CREATE TABLE OATC (
+	AbsEntry 	 INT 		   PRIMARY KEY
+);
---
+++ src/main/resources/db/migration/primary/V1455453888__PRIMARY_MODEL_INSERT.sql
@@ -0,0 +1,8 @@
+/*
+ * 200:孙哲,201:崔浩然,229:刘明明
+ */
+
+update OUSR set FAX=NULL;
+update OUSR set FAX='229' where USERID = 19;
+update OUSR set FAX='200' where USERID = 25;
+update OUSR set FAX='201' where USERID = 28; 
