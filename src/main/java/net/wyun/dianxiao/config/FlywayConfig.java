973605afc8d832cd213ac3d179088294066a11c7
michaelyin <gt8442b@yahoo.com>
2016-02-13 22:05:09 -0500

    diable flyway. may not work. TODO.

--- src/main/java/net/wyun/dianxiao/config/FlywayConfig.java
+++ src/main/java/net/wyun/dianxiao/config/FlywayConfig.java
@@ -2,7 +2,11 @@ package net.wyun.dianxiao.config;
 
 import javax.sql.DataSource;
 
+import net.wyun.dianxiao.service.primary.MP3FileHandlerImpl;
+
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -14,6 +18,8 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class FlywayConfig {
 
+	private static final Logger logger = LoggerFactory.getLogger(FlywayConfig.class);
+	
 	@Autowired
 	@Qualifier(PrimaryDataSourceConfig.PREFIX_PRIMARY)
 	private DataSource primaryDataSource;
@@ -25,16 +31,19 @@ public class FlywayConfig {
 	@Autowired
 	private FlywayConfiguration flywayConfiguration;
 
-	@Bean(initMethod = "migrate")
+	
 	@ConditionalOnProperty(prefix = "flyway", name = "enabled", matchIfMissing = true)
+	@Bean(initMethod = "migrate")
 	@FlywayDataSource
 	@Primary
 	public Flyway primaryFlyway() {
+		logger.debug("generate primary flyway here");
 		Flyway flyway = new Flyway();
 		
 		flyway.setDataSource(primaryDataSource);
 		flyway.setLocations(flywayConfiguration.getPrimary().getLocation());
 		flyway.setEncoding("GBK");
+		logger.debug("primary flyway generated here");
 		return flyway;
 	}
 	
