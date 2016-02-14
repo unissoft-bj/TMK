d01b87fba66c31e0074165234495f181e03d41ef
michaelyin <gt8442b@yahoo.com>
2016-02-13 21:53:37 -0500

    add query to get max atcEntry from table.

--- src/main/java/net/wyun/dianxiao/repository/primary/OCLGRepository.java
+++ src/main/java/net/wyun/dianxiao/repository/primary/OCLGRepository.java
@@ -1,8 +1,6 @@
 package net.wyun.dianxiao.repository.primary;
 
 import net.wyun.dianxiao.model.primary.OCLG;
-import net.wyun.dianxiao.model.primary.PrimaryModel;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -11,4 +9,7 @@ public interface OCLGRepository extends CrudRepository<OCLG, Integer>{
 
 	@Query("select coalesce(max(o.clgCode), '0') from OCLG o")
 	int findMaxClgCode();
+	
+	@Query("select coalesce(max(o.atcEntry), '0') from OCLG o")
+	int findMaxAtcEntry();
 }
