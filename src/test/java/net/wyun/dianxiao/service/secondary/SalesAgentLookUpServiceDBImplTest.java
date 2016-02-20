b75faf327bd40b333e720449a6df753c1166d5d0
michaelyin <gt8442b@yahoo.com>
2016-02-19 19:38:07 -0500

    add more test.

--- src/test/java/net/wyun/dianxiao/service/secondary/SalesAgentLookUpServiceDBImplTest.java
+++ src/test/java/net/wyun/dianxiao/service/secondary/SalesAgentLookUpServiceDBImplTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import net.wyun.dianxiao.BaseSpringTestRunner;
 import net.wyun.dianxiao.model.CallDirection;
+import net.wyun.dianxiao.model.primary.OUSR;
 import net.wyun.dianxiao.repository.primary.OUSRRepository;
 
 import org.junit.Test;
@@ -33,6 +34,51 @@ public class SalesAgentLookUpServiceDBImplTest extends BaseSpringTestRunner{
 		list.add("200");
 		CallDirection direction = lookupSvc.getCallDirection(list);
 		assertTrue(direction == CallDirection.IN);
+	}
+	
+	@Test
+	public void testGetCallDirection3() {
+		List<String> list = new ArrayList<String>();;
+		list.add("229");
+		list.add("200");
+		CallDirection direction = lookupSvc.getCallDirection(list);
+		assertTrue(direction == CallDirection.OUT);
+	}
+	
+	@Test
+	public void testGetCallDirection4() {
+		List<String> list = new ArrayList<String>();;
+		list.add("18333531611");
+		list.add("900");
+		CallDirection direction = lookupSvc.getCallDirection(list);
+		assertTrue(direction == CallDirection.NOTSET);
+	}
+	
+	@Test
+	public void testSelect(){
+		List<OUSR> list = new ArrayList<OUSR>();
+		OUSR ou1 = new OUSR();
+		ou1.setLocked("N");
+		ou1.setUSERID((short) 77);
+		
+		OUSR ou2 = new OUSR();
+		ou2.setLocked("N");
+		ou2.setUSERID((short) 88);
+		
+		OUSR ou3 = new OUSR();
+		ou3.setLocked("N");
+		ou3.setUSERID((short) 66);
+		
+		OUSR ou4 = new OUSR();
+		ou4.setLocked("Y");
+		ou4.setUSERID((short) 99);
+		
+		list.add(ou1); list.add(ou2); list.add(ou3); list.add(ou4);
+		
+		OUSR topOUSR = lookupSvc.select(list);
+		System.out.println("USERID: " + topOUSR.getUSERID());
+		short expected = 88;
+		assertEquals(expected, topOUSR.getUSERID());
 	}
 
 }
